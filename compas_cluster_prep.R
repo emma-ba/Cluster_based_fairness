2e4230bd141fa70b027440bac42e0f36151ae9a8
emma-ba <ebeauxis@gmail.com>
2024-05-17 11:02:43 +0200

    Data preparation as classification problem

--- compas_cluster_prep.R
+++ compas_cluster_prep.R
@@ -24,13 +24,28 @@ data_orig %>%
 ##### PREP DATA #############
 data <- data_orig %>% 
   mutate(priors_count = priors_count.1) %>%
-  select(decile_score, is_recid,
-         sex, age, race, 
+  select(sex, age, race, 
          juv_fel_count, juv_misd_count, juv_other_count, priors_count,
-         c_charge_degree #, id
+         c_charge_degree, 
+         decile_score, is_recid,
          ) %>% 
   drop_na
 
 data %>% glimpse
 
 write_csv(data, 'compas_clustering_prep.csv')
+
+##### PREP AS CLASSIFICATION #############
+thres <- 5.5
+
+data_classpb <- data %>% 
+  mutate(pred_recid = ifelse(decile_score < thres, 0, 1)) %>% 
+  mutate(TP = ifelse(pred_recid == 1 & is_recid == 1, 1, 0)) %>%
+  mutate(TN = ifelse(pred_recid == 0 & is_recid == 0, 1, 0)) %>%
+  mutate(FP = ifelse(pred_recid == 1 & is_recid == 0, 1, 0)) %>%
+  mutate(FN = ifelse(pred_recid == 0 & is_recid == 1, 1, 0)) %>%
+  glimpse
+
+data_classpb %>% glimpse
+
+write_csv(data_classpb, 'compas_clustering_classpb.csv')
